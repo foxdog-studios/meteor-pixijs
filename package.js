fa92909787ead814598851ecbacafcda20cb94af
Peter Sutton <foxxy@foxdogstudios.com>
2015-04-19 15:48:05 +0100

    Updated to v2.2.8. Closes #1

--- package.js
+++ package.js
@@ -3,17 +3,18 @@
 Package.describe({
   summary: '2D webGL renderer with canvas fallback',
   name: 'fds:pixijs',
-  version: '1.5.1_1',
+  version: '2.2.8_1',
+  documentation: 'README.md',
   git: 'https://github.com/foxdog-studios/meteor-pixijs.git'
 });
 
 Package.onUse(function (api) {
-  api.versionsFrom('1.0');
+  api.versionsFrom('METEOR@1.1.0.2');
   api.export('PIXI', 'client');
   api.addFiles(['lib/pixijs/bin/pixi.js', 'export-pixi.js'], 'client');
 });
 
 Package.onTest(function (api) {
-  api.use('fds:pixijs');
+  api.use('fds:pixijs')
 });
 
